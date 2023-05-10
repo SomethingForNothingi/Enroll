f18d58970c1ae1c3657c04ea7868473903fc4718
宋文博 <1021102104@qq.com>
2023-05-10 21:55:00 +0800

    update

--- app/Http/Controllers/AdminLoginController.php
+++ app/Http/Controllers/AdminLoginController.php
@@ -5,29 +5,29 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class   AdminLoginController extends Controller
+class AdminLoginController extends Controller
 {
     public function login(Request $request)
     {
         $params = $request->all();
         // 验证码
         $captcha = $this->validateCaptcha($params['key'], $params['captcha']);
-        if(!$captcha) {
-            return response($this->returnData(self::FAIL,'验证码错误'));
+        if (!$captcha) {
+            return response($this->returnData(self::FAIL, '验证码错误'));
         }
         //验证
-        $credentials =[
+        $credentials = [
             'username' => $params['username'],
             'password' => $params['password']
         ];
         $token = Auth::guard(self::GUARD_ADMIN)->attempt($credentials);
         if (!$token) {
-            return $this->returnData(self::FAIL,'账户信息错误');
+            return $this->returnData(self::FAIL, '账户信息错误');
         }
 
         $userData = Auth::guard(self::GUARD_ADMIN)->user();
 
-        return $this->returnData(self::OK, '登陆成功', ['token' => $token, 'user' => $userData]);
+        return $this->returnData(self::OK, '登陆成功', [ 'token' => $token, 'user' => $userData ]);
     }
 
     public function logout(): array
@@ -42,16 +42,17 @@ class   AdminLoginController extends Controller
     public function getCaptcha()
     {
         return response(
-            $this->returnData(self::OK,'获取验证成功',app('captcha')->create('default', true))
+            $this->returnData(self::OK, '获取验证成功', app('captcha')->create('default', true))
         );
 
     }
+
     /**
      * 验证验证码
      */
-    public function validateCaptcha($key , $captcha): bool
+    public function validateCaptcha($key, $captcha): bool
     {
-        if (captcha_api_check($captcha , $key)){
+        if (captcha_api_check($captcha, $key)) {
             return true;
         } else {
             return false;
